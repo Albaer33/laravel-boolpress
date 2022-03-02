2751db26318bcfe8f608f61e6c35a3e9583c602f
Albaer33 <alisi3396@gmail.com>
2022-03-02 14:34:12 +0100

    api controller created

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(6);
+        $posts = Post::paginate(8);
         
         return view('admin.posts.index', compact('posts'));
     }
--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,9 +1,9 @@
 <template>
-    <h1>work in progress</h1>
+    <div>bfdsbdsbdsbfds</div>
 </template>
 
 <script>
 export default {
-    name: 'WorkInProgress'
+    name: 'Posts',
 }
 </script>
